550142e2cedb58ef1ea978d1f21dfc7507c1fa5c
Luc <34852@ma-web.nl>
2022-11-22 12:37:53 +0100

    opdracht 1

---
+++ les 1/js/main.js
@@ -0,0 +1,17 @@
+class App
+{
+    runApplication()
+    {
+        console.log("hello world");
+        let appNaam = ("string");
+        let verison = 0.9;
+        let versiedatum = ("22/11/2022");
+        let auteur = ("carl");
+        let copyright = ("go fuck yourself");
+        let distribiteur = ("steam");
+        let darkmode = true;
+        console.log(appNaam, verison, versiedatum, auteur, copyright, distribiteur, darkmode)
+    }
+}
+let app = new App();
+app.runApplication();
